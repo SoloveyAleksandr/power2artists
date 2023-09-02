a55fbe9d1f8b0e22dc0b44d724933655a725bb93
Aleksandr <solalex.s.a@gmail.com>
2023-09-02 15:43:22 +0300

    first commit

---
+++ dev/js/scripts/utils.js
@@ -0,0 +1,7 @@
+const isMobileDevice = () => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+    navigator.userAgent
+  )
+    ? true
+    : false;
+};
